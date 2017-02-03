89163071820bf271036a6e92290d43254f35dc6c
Tony Hillerson <tony.hillerson@mapquest.com>
2017-02-03 15:01:52 -0700

    cutting back to a known good state

--- practice/src/com/company/dataStructures/arrays/MergeSort.java
+++ practice/src/com/company/dataStructures/arrays/MergeSort.java
@@ -19,7 +19,6 @@ public class MergeSort {
   }
 
   private static void merge(int[] arr, int l, int m, int r) {
-    int offset, rightIndex;
     int leftLength = m - l + 1,
         rightLength = r - m;
     System.out.printf("arr: %s\n", Arrays.toString(arr));
@@ -27,29 +26,11 @@ public class MergeSort {
     int[] temporaryLeft = new int[leftLength],
           temporaryRight = new int[rightLength];
 
-    for (int i = l; i <= m; i++) {
-      offset = l + i;
-      rightIndex = m + 1 + offset;
-      temporaryLeft[offset] = arr[i];
-      if (rightIndex <= r && rightIndex < arr.length) {
-        temporaryRight[offset] = arr[rightIndex];
-      }
+    for (int i = 0; i < leftLength; i++) {
+      temporaryLeft[i] = arr[l + i];
     }
-
-    int left, right, leftPointer = 0, rightPointer = 0, mergePointer = l;
-    while (leftPointer <= m && rightPointer <= r) {
-      System.out.printf("left pointer: %s, right pointer %s\n", leftPointer, rightPointer);
-      left = temporaryLeft[leftPointer];
-      right = temporaryRight[rightPointer];
-      if (left < right) {
-        arr[mergePointer++] = left;
-        arr[mergePointer++] = right;
-      } else {
-        arr[mergePointer++] = right;
-        arr[mergePointer++] = left;
-      }
-      leftPointer++;
-      rightPointer++;
+    for (int i = 0; i < rightLength; i++) {
+      temporaryRight[i] = arr[m + i + i];
     }
   }
 }
